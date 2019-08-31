aa12d322413fa17de238848f22eeb8bc751567ab
Kevin <stradtkt22@gmail.com>
2019-08-31 08:54:49 -0400

    Added AppDatabase

--- app/src/main/java/com/stradtman/tasktimer/AppDatabase.java
+++ app/src/main/java/com/stradtman/tasktimer/AppDatabase.java
@@ -1,5 +1,45 @@
 package com.stradtman.tasktimer;
 
-class AppDatabase {
-    
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Basic database class for the app
+ * The only class that should use this is the AppProvider class.
+ */
+class AppDatabase extends SQLiteOpenHelper {
+    private static final String TAG = "AppDatabase";
+    public static final String DATABASE_NAME = "TaskTimer.db";
+    public static final int DATABASE_VERSION = 1;
+    private static AppDatabase instance = null;
+
+    private AppDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d(TAG, "AppDatabase: constructor");
+    }
+
+    static AppDatabase getInstance(Context context) {
+        if(instance == null) {
+            Log.d(TAG, "getInstance: creating new instance");
+            instance = new AppDatabase(context);
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(TAG, "onCreate: starts");
+        String sSQL;
+        sSQL = "CREATE TABLE Tasks (_id INTEGER PRIMARY KEY NOT NULL, Name TEXT NOT NULL, Description TEXT, SortOrder INTEGER, CategoryID INTEGER);";
+        Log.d(TAG, "sql: " + sSQL);
+        db.execSQL(sSQL);
+        Log.d(TAG, "onCreate: ends");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
